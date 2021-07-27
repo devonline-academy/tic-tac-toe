3e2e72f2924f4674a289f864cf5b4244f799ca6a
devonline <devonline.academy@gmail.com>
2021-07-27 17:03:19 +0300

    Override toString methods for model classes

--- src/academy/devonline/tictactoe/model/Cell.java
+++ src/academy/devonline/tictactoe/model/Cell.java
@@ -39,4 +39,12 @@ public class Cell {
     public int getCol() {
         return col;
     }
+
+    @Override
+    public String toString() {
+        return "Cell{" +
+                "row=" + row +
+                ", col=" + col +
+                '}';
+    }
 }
--- src/academy/devonline/tictactoe/model/GameTable.java
+++ src/academy/devonline/tictactoe/model/GameTable.java
@@ -17,6 +17,8 @@
 
 package academy.devonline.tictactoe.model;
 
+import java.util.Arrays;
+
 /**
  * @author devonline
  * @link http://devonline.academy/java
@@ -39,5 +41,19 @@ public class GameTable {
 
     public void setSign(final Cell cell, final char sign) {
         table[cell.getRow()][cell.getCol()] = sign;
+    }
+
+    @Override
+    public String toString() {
+        final StringBuilder sb = new StringBuilder("GameTable{");
+        sb.append("table=");
+        for (int i = 0; i < table.length; i++) {
+            sb.append(Arrays.toString(table[i]));
+            if (i < table.length - 1) {
+                sb.append(';');
+            }
+        }
+        sb.append('}');
+        return sb.toString();
     }
 }
