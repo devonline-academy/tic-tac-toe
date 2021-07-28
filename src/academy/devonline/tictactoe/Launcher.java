102a736957c26de9751f50124862f90cd59fc1b6
devonline <devonline.academy@gmail.com>
2021-07-28 12:35:26 +0300

    Use DesktopNumericKeypadCellConverter by default

--- src/academy/devonline/tictactoe/Launcher.java
+++ src/academy/devonline/tictactoe/Launcher.java
@@ -24,7 +24,7 @@ import academy.devonline.tictactoe.component.DataPrinter;
 import academy.devonline.tictactoe.component.Game;
 import academy.devonline.tictactoe.component.UserMove;
 import academy.devonline.tictactoe.component.WinnerVerifier;
-import academy.devonline.tictactoe.component.keypad.TerminalNumericKeypadCellNumberConverter;
+import academy.devonline.tictactoe.component.keypad.DesktopNumericKeypadCellNumberConverter;
 
 /**
  * @author devonline
@@ -33,7 +33,7 @@ import academy.devonline.tictactoe.component.keypad.TerminalNumericKeypadCellNum
 public final class Launcher {
 
     public static void main(final String[] args) {
-        final CellNumberConverter cellNumberConverter = new TerminalNumericKeypadCellNumberConverter();
+        final CellNumberConverter cellNumberConverter = new DesktopNumericKeypadCellNumberConverter();
         final Game game = new Game(
                 new DataPrinter(cellNumberConverter),
                 new ComputerMove(),
