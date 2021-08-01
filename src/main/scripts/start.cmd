b73fa4dae694fbda75a4c4adb4d3e0168ab98ba2
devonline <devonline.academy@gmail.com>
2021-08-01 14:08:25 +0300

    Update start scripts

--- src/main/scripts/start.cmd
+++ src/main/scripts/start.cmd
@@ -19,6 +19,40 @@
 @rem @email  devonline.academy@gmail.com
 @rem ###################################################################################################################
 @echo off
-
-java -jar ${project.build.finalName}-release.jar
+@rem -------------------------------------------------------------------------------------------------------------------
+setlocal
+@rem -------------------------------------------------------------------------------------------------------------------
+@rem Try to use the `java.exe` from `%JAVA_HOME%` if this environment variable set correctly:
+if "%JAVA_HOME%" neq "" (
+    if exist %JAVA_HOME%\bin\java.exe (
+        set JAVA_CMD=%JAVA_HOME%\bin\java
+    )
+)
+@rem -------------------------------------------------------------------------------------------------------------------
+@rem Try to use the `java.exe` using `PATH` environment variable:
+where /Q java
+if %ERRORLEVEL% == 0 (
+    set JAVA_CMD=java
+)
+@rem -------------------------------------------------------------------------------------------------------------------
+@rem Try to use `java.exe` from JRE if `jre\bin\java.exe` exists:
+if exist jre\bin\java.exe (
+    set JAVA_CMD=jre\bin\java
+)
+@rem -------------------------------------------------------------------------------------------------------------------
+if not defined JAVA_CMD (
+    @rem Throw error if `java.exe` is not configured:
+    echo -------------------------------------------------------------------------- >&2
+    echo `java.exe` not defined! Install or configure JVM before using this script! >&2
+    echo -------------------------------------------------------------------------- >&2
+    set RETURN_CODE=1
+) else (
+    @rem Run tic-tac-toe game:
+    %JAVA_CMD% -jar ${project.build.finalName}-release.jar
+    set RETURN_CODE=0
+)
+@rem -------------------------------------------------------------------------------------------------------------------
+@rem Wait for the any key pressed:
 pause
+@rem -------------------------------------------------------------------------------------------------------------------
+exit /b %RETURN_CODE%
--- src/main/scripts/start.sh
+++ src/main/scripts/start.sh
@@ -19,8 +19,40 @@
 # @author devonline                                                                                                    #
 # @email  devonline.academy@gmail.com                                                                                  #
 ########################################################################################################################
-cd "$(dirname "$0")" || exit
-
-java -jar ${project.build.finalName}-release.jar
+# ----------------------------------------------------------------------------------------------------------------------
+# Try to use the `java` from `$JAVA_HOME` if this environment variable set correctly:
+if [ -n "$JAVA_HOME" ] && [ -x "$JAVA_HOME/bin/java" ] ; then
+  JAVA_CMD="$JAVA_HOME/bin/java"
+fi
+# ----------------------------------------------------------------------------------------------------------------------
+# Try to use the `java` using `PATH` environment variable:
+WHICH_JAVA=$(which java)
+if [ -x "$WHICH_JAVA" ]; then
+  JAVA_CMD="java"
+  unset WHICH_JAVA
+fi
+# ----------------------------------------------------------------------------------------------------------------------
+# Try to use `java` from JRE if `jre/bin/java` exists and executable:
+if [ -x "jre/bin/java" ]; then
+  JAVA_CMD="jre/bin/java"
+fi
+# ----------------------------------------------------------------------------------------------------------------------
+if [ -z ${JAVA_CMD+x} ]; then
+  # Throw error if `java` is not configured:
+  echo "------------------------------------------------------------------------" >&2
+  echo "\`java\` not defined! Install or configure JVM before using this script!" >&2
+  echo "------------------------------------------------------------------------" >&2
+  RETURN_CODE=1
+else
+  # Fix current dir issue. Read more: http://hints.macworld.com/article.php?story=20041217111834902
+  cd "$(dirname "$0")" || exit
+  # Run tic-tac-toe game:
+  $JAVA_CMD -jar ${project.build.finalName}-release.jar
+  RETURN_CODE=0
+fi
+# ----------------------------------------------------------------------------------------------------------------------
+# Wait for the `enter` key pressed:
 echo "Press enter to continue . . ."
 read -r test
+# ----------------------------------------------------------------------------------------------------------------------
+exit $RETURN_CODE
