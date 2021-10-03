489af4a5c30ce25f14fbab3ec7cb736b3aede270
devonline <devonline.academy@gmail.com>
2021-10-03 19:11:52 +0300

    Add UserInterface command line argument

--- src/main/java/academy/devonline/tictactoe/component/CommandLineArgumentParser.java
+++ src/main/java/academy/devonline/tictactoe/component/CommandLineArgumentParser.java
@@ -18,9 +18,12 @@
 package academy.devonline.tictactoe.component;
 
 import academy.devonline.tictactoe.model.PlayerType;
+import academy.devonline.tictactoe.model.UserInterface;
 
 import static academy.devonline.tictactoe.model.PlayerType.COMPUTER;
 import static academy.devonline.tictactoe.model.PlayerType.USER;
+import static academy.devonline.tictactoe.model.UserInterface.CONSOLE;
+import static academy.devonline.tictactoe.model.UserInterface.GUI;
 
 /**
  * @author devonline
@@ -34,9 +37,10 @@ public class CommandLineArgumentParser {
         this.args = args;
     }
 
-    public PlayerTypes parse() {
+    public CommandLineArguments parse() {
         PlayerType player1Type = null;
         PlayerType player2Type = null;
+        UserInterface userInterface = null;
         for (final String arg : args) {
             if (USER.name().equalsIgnoreCase(arg) || COMPUTER.name().equalsIgnoreCase(arg)) {
                 if (player1Type == null) {
@@ -46,16 +50,25 @@ public class CommandLineArgumentParser {
                 } else {
                     System.err.println("Unsupported command line argument: '" + arg + "'");
                 }
+            } else if (GUI.name().equalsIgnoreCase(arg) || CONSOLE.name().equalsIgnoreCase(arg)) {
+                if (userInterface == null) {
+                    userInterface = UserInterface.valueOf(arg.toUpperCase());
+                } else {
+                    System.err.println("Unsupported command line argument: '" + arg + "'");
+                }
             } else {
                 System.err.println("Unsupported command line argument: '" + arg + "'");
             }
         }
+        if (userInterface == null) {
+            userInterface = CONSOLE;
+        }
         if (player1Type == null) {
-            return new PlayerTypes(USER, COMPUTER);
+            return new CommandLineArguments(USER, COMPUTER, userInterface);
         } else if (player2Type == null) {
-            return new PlayerTypes(USER, player1Type);
+            return new CommandLineArguments(USER, player1Type, userInterface);
         } else {
-            return new PlayerTypes(player1Type, player2Type);
+            return new CommandLineArguments(player1Type, player2Type, userInterface);
         }
     }
 
@@ -63,16 +76,20 @@ public class CommandLineArgumentParser {
      * @author devonline
      * @link http://devonline.academy/java
      */
-    public static class PlayerTypes {
+    public static class CommandLineArguments {
 
         private final PlayerType player1Type;
 
         private final PlayerType player2Type;
 
-        private PlayerTypes(final PlayerType player1Type,
-                            final PlayerType player2Type) {
+        private final UserInterface userInterface;
+
+        private CommandLineArguments(final PlayerType player1Type,
+                                     final PlayerType player2Type,
+                                     final UserInterface userInterface) {
             this.player1Type = player1Type;
             this.player2Type = player2Type;
+            this.userInterface = userInterface;
         }
 
         public PlayerType getPlayer1Type() {
@@ -81,6 +98,10 @@ public class CommandLineArgumentParser {
 
         public PlayerType getPlayer2Type() {
             return player2Type;
+        }
+
+        public UserInterface getUserInterface() {
+            return userInterface;
         }
     }
 }
--- src/main/java/academy/devonline/tictactoe/component/GameFactory.java
+++ src/main/java/academy/devonline/tictactoe/component/GameFactory.java
@@ -17,13 +17,18 @@
 
 package academy.devonline.tictactoe.component;
 
+import academy.devonline.tictactoe.component.console.ConsoleDataPrinter;
+import academy.devonline.tictactoe.component.console.ConsoleUserInputReader;
+import academy.devonline.tictactoe.component.keypad.DesktopNumericKeypadCellNumberConverter;
 import academy.devonline.tictactoe.component.swing.GameWindow;
 import academy.devonline.tictactoe.model.Player;
 import academy.devonline.tictactoe.model.PlayerType;
+import academy.devonline.tictactoe.model.UserInterface;
 
 import static academy.devonline.tictactoe.model.PlayerType.USER;
 import static academy.devonline.tictactoe.model.Sign.O;
 import static academy.devonline.tictactoe.model.Sign.X;
+import static academy.devonline.tictactoe.model.UserInterface.GUI;
 
 /**
  * @author devonline
@@ -35,19 +40,28 @@ public class GameFactory {
 
     private final PlayerType player2Type;
 
+    private final UserInterface userInterface;
+
     public GameFactory(final String[] args) {
-        final CommandLineArgumentParser.PlayerTypes playerTypes =
+        final CommandLineArgumentParser.CommandLineArguments commandLineArguments =
                 new CommandLineArgumentParser(args).parse();
-        this.player1Type = playerTypes.getPlayer1Type();
-        this.player2Type = playerTypes.getPlayer2Type();
+        player1Type = commandLineArguments.getPlayer1Type();
+        player2Type = commandLineArguments.getPlayer2Type();
+        userInterface = commandLineArguments.getUserInterface();
     }
 
     public Game create() {
-        final GameWindow gameWindow = new GameWindow();
-        //final CellNumberConverter cellNumberConverter = new DesktopNumericKeypadCellNumberConverter();
-        final DataPrinter dataPrinter = gameWindow;//new ConsoleDataPrinter(cellNumberConverter);
-        final UserInputReader userInputReader = gameWindow;//new ConsoleUserInputReader(cellNumberConverter, dataPrinter);
-
+        final DataPrinter dataPrinter;
+        final UserInputReader userInputReader;
+        if (userInterface == GUI) {
+            final GameWindow gameWindow = new GameWindow();
+            dataPrinter = gameWindow;
+            userInputReader = gameWindow;
+        } else {
+            final CellNumberConverter cellNumberConverter = new DesktopNumericKeypadCellNumberConverter();
+            dataPrinter = new ConsoleDataPrinter(cellNumberConverter);
+            userInputReader = new ConsoleUserInputReader(cellNumberConverter, dataPrinter);
+        }
         final Player player1;
         if (player1Type == USER) {
             player1 = new Player(X, new UserMove(userInputReader, dataPrinter));
