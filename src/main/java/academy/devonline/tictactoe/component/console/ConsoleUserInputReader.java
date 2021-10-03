38d55875c41805ad7303bf442d4454c57745ca8a
devonline <devonline.academy@gmail.com>
2021-10-03 18:31:04 +0300

    Implement console user interface classes

---
+++ src/main/java/academy/devonline/tictactoe/component/console/ConsoleUserInputReader.java
@@ -0,0 +1,56 @@
+/*
+ * Copyright (c) 2019. http://devonline.academy
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ *
+ */
+
+package academy.devonline.tictactoe.component.console;
+
+import academy.devonline.tictactoe.component.CellNumberConverter;
+import academy.devonline.tictactoe.component.DataPrinter;
+import academy.devonline.tictactoe.component.UserInputReader;
+import academy.devonline.tictactoe.model.Cell;
+
+import java.util.Scanner;
+
+/**
+ * @author devonline
+ * @link http://devonline.academy/java
+ */
+public class ConsoleUserInputReader implements UserInputReader {
+
+    private final CellNumberConverter cellNumberConverter;
+
+    private final DataPrinter dataPrinter;
+
+    public ConsoleUserInputReader(final CellNumberConverter cellNumberConverter,
+                                  final DataPrinter dataPrinter) {
+        this.cellNumberConverter = cellNumberConverter;
+        this.dataPrinter = dataPrinter;
+    }
+
+    @Override
+    public Cell getUserInput() {
+        while (true) {
+            dataPrinter.printInfoMessage("Please type number between 1 and 9:");
+            final String userInput = new Scanner(System.in).nextLine();
+            if (userInput.length() == 1) {
+                final char ch = userInput.charAt(0);
+                if (ch >= '1' && ch <= '9') {
+                    return cellNumberConverter.toCell(ch);
+                }
+            }
+        }
+    }
+}
