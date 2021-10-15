94a8f36ff2da43d671a79ca6c005dca7d6c16a79
devonline <devonline.academy@gmail.com>
2021-10-15 18:09:41 +0300

    Simplify PreventUserWinComputerMoveStrategy code

---
+++ src/main/java/academy/devonline/tictactoe/component/strategy/AbstractComputerMoveStrategy.java
@@ -0,0 +1,87 @@
+package academy.devonline.tictactoe.component.strategy;
+
+import academy.devonline.tictactoe.component.ComputerMoveStrategy;
+import academy.devonline.tictactoe.model.game.Cell;
+import academy.devonline.tictactoe.model.game.GameTable;
+import academy.devonline.tictactoe.model.game.Sign;
+
+/**
+ * @author devonline
+ * @link http://devonline.academy/java
+ */
+public abstract class AbstractComputerMoveStrategy implements ComputerMoveStrategy {
+
+    @Override
+    public final boolean tryToMakeMove(final GameTable gameTable, final Sign moveSign) {
+        final Sign findSign = getFindSign(moveSign);
+        return tryToMakeMoveByRows(gameTable, findSign, moveSign) ||
+                tryToMakeMoveByCols(gameTable, findSign, moveSign) ||
+                tryToMakeMoveByMainDiagonal(gameTable, findSign, moveSign) ||
+                tryToMakeMoveBySecondaryDiagonal(gameTable, findSign, moveSign);
+    }
+
+    protected abstract Sign getFindSign(Sign moveSign);
+
+    @SuppressWarnings("Convert2MethodRef")
+    private boolean tryToMakeMoveByRows(final GameTable gameTable, final Sign findSign, final Sign moveSign) {
+        for (int i = 0; i < 3; i++) {
+            if (tryToMakeMoveUsingLambdaConversion(gameTable, findSign, moveSign, i, (k, j) -> new Cell(k, j))) {
+                return true;
+            }
+        }
+        return false;
+    }
+
+    private boolean tryToMakeMoveByCols(final GameTable gameTable, final Sign findSign, final Sign moveSign) {
+        for (int i = 0; i < 3; i++) {
+            if (tryToMakeMoveUsingLambdaConversion(gameTable, findSign, moveSign, i, (k, j) -> new Cell(j, k))) {
+                return true;
+            }
+        }
+        return false;
+    }
+
+    private boolean tryToMakeMoveByMainDiagonal(final GameTable gameTable, final Sign findSign, final Sign moveSign) {
+        return tryToMakeMoveUsingLambdaConversion(gameTable, findSign, moveSign, -1, (k, j) -> new Cell(j, j));
+    }
+
+    private boolean tryToMakeMoveBySecondaryDiagonal(final GameTable gameTable, final Sign findSign, final Sign moveSign) {
+        return tryToMakeMoveUsingLambdaConversion(gameTable, findSign, moveSign, -1, (k, j) -> new Cell(j, 2 - j));
+    }
+
+    private boolean tryToMakeMoveUsingLambdaConversion(final GameTable gameTable,
+                                                       final Sign findSign,
+                                                       final Sign moveSign,
+                                                       final int i,
+                                                       final Lambda lambda) {
+        int countEmptyCells = 0;
+        int countSignCells = 0;
+        Cell lastEmptyCell = null;
+        for (int j = 0; j < 3; j++) {
+            final Cell cell = lambda.convert(i, j);
+            if (gameTable.isEmpty(cell)) {
+                lastEmptyCell = cell;
+                countEmptyCells++;
+            } else if (gameTable.getSign(cell) == findSign) {
+                countSignCells++;
+            } else {
+                break;
+            }
+        }
+        if (countEmptyCells == 1 && countSignCells == 2) {
+            gameTable.setSign(lastEmptyCell, moveSign);
+            return true;
+        }
+        return false;
+    }
+
+    /**
+     * @author devonline
+     * @link http://devonline.academy/java
+     */
+    @FunctionalInterface
+    private interface Lambda {
+
+        Cell convert(int k, int j);
+    }
+}
--- src/main/java/academy/devonline/tictactoe/component/strategy/PreventUserWinComputerMoveStrategy.java
+++ src/main/java/academy/devonline/tictactoe/component/strategy/PreventUserWinComputerMoveStrategy.java
@@ -1,84 +1,16 @@
 package academy.devonline.tictactoe.component.strategy;
 
-import academy.devonline.tictactoe.component.ComputerMoveStrategy;
-import academy.devonline.tictactoe.model.game.Cell;
-import academy.devonline.tictactoe.model.game.GameTable;
 import academy.devonline.tictactoe.model.game.Sign;
 
 /**
  * @author devonline
  * @link http://devonline.academy/java
  */
-public class PreventUserWinComputerMoveStrategy implements ComputerMoveStrategy {
+public class PreventUserWinComputerMoveStrategy extends AbstractComputerMoveStrategy {
 
     @Override
-    public boolean tryToMakeMove(final GameTable gameTable, final Sign sign) {
-        return tryToMakeMoveByRows(gameTable, sign) ||
-                tryToMakeMoveByCols(gameTable, sign) ||
-                tryToMakeMoveByMainDiagonal(gameTable, sign) ||
-                tryToMakeMoveBySecondaryDiagonal(gameTable, sign);
-    }
-
-    @SuppressWarnings("Convert2MethodRef")
-    private boolean tryToMakeMoveByRows(final GameTable gameTable, final Sign sign) {
-        for (int i = 0; i < 3; i++) {
-            if (tryToMakeMoveUsingLambdaConversion(gameTable, sign, i, (k, j) -> new Cell(k, j))) {
-                return true;
-            }
-        }
-        return false;
-    }
-
-    private boolean tryToMakeMoveByCols(final GameTable gameTable, final Sign sign) {
-        for (int i = 0; i < 3; i++) {
-            if (tryToMakeMoveUsingLambdaConversion(gameTable, sign, i, (k, j) -> new Cell(j, k))) {
-                return true;
-            }
-        }
-        return false;
-    }
-
-    private boolean tryToMakeMoveByMainDiagonal(final GameTable gameTable, final Sign sign) {
-        return tryToMakeMoveUsingLambdaConversion(gameTable, sign, -1, (k, j) -> new Cell(j, j));
-    }
-
-    private boolean tryToMakeMoveBySecondaryDiagonal(final GameTable gameTable, final Sign sign) {
-        return tryToMakeMoveUsingLambdaConversion(gameTable, sign, -1, (k, j) -> new Cell(j, 2 - j));
-    }
-
-    private boolean tryToMakeMoveUsingLambdaConversion(final GameTable gameTable,
-                                                       final Sign sign,
-                                                       final int i,
-                                                       final Lambda lambda) {
-        int countEmptyCells = 0;
-        int countSignCells = 0;
-        Cell lastEmptyCell = null;
-        for (int j = 0; j < 3; j++) {
-            final Cell cell = lambda.convert(i, j);
-            if (gameTable.isEmpty(cell)) {
-                lastEmptyCell = cell;
-                countEmptyCells++;
-            } else if (gameTable.getSign(cell) == sign.oppositeSign()) {
-                countSignCells++;
-            } else {
-                break;
-            }
-        }
-        if (countEmptyCells == 1 && countSignCells == 2) {
-            gameTable.setSign(lastEmptyCell, sign);
-            return true;
-        }
-        return false;
-    }
-
-    /**
-     * @author devonline
-     * @link http://devonline.academy/java
-     */
-    @FunctionalInterface
-    private interface Lambda {
-
-        Cell convert(int k, int j);
+    protected Sign getFindSign(final Sign moveSign) {
+        return moveSign.oppositeSign();
     }
 }
 
