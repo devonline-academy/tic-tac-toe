09bd71d89e64e72a0f399382a66f8e30c2f3369c
devonline <devonline.academy@gmail.com>
2021-10-15 16:09:08 +0300

    Replace infinite loop for ComputerMove class

--- src/main/java/academy/devonline/tictactoe/component/ComputerMove.java
+++ src/main/java/academy/devonline/tictactoe/component/ComputerMove.java
@@ -31,15 +31,21 @@ public class ComputerMove implements Move {
 
     @Override
     public void make(final GameTable gameTable, final Sign sign) {
-        final Random random = new Random();
-        while (true) {
-            final int row = random.nextInt(3);
-            final int col = random.nextInt(3);
-            final Cell randomCell = new Cell(row, col);
-            if (gameTable.isEmpty(randomCell)) {
-                gameTable.setSign(randomCell, sign);
-                return;
+        final Cell[] emptyCells = new Cell[9];
+        int count = 0;
+        for (int i = 0; i < 3; i++) {
+            for (int j = 0; j < 3; j++) {
+                final Cell cell = new Cell(i, j);
+                if (gameTable.isEmpty(cell)) {
+                    emptyCells[count++] = cell;
+                }
             }
+        }
+        if (count > 0) {
+            final Cell randomCell = emptyCells[new Random().nextInt(count)];
+            gameTable.setSign(randomCell, sign);
+        } else {
+            throw new IllegalArgumentException("Game table does not contain any empty cell!");
         }
     }
 }
