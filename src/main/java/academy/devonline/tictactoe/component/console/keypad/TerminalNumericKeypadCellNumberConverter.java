23d5fc27b6e9ee424db1087f903fe31d22c4f02e
devonline <devonline.academy@gmail.com>
2021-10-15 15:07:38 +0300

    Fix toCell & toNumber methods for terminal impl

--- src/main/java/academy/devonline/tictactoe/component/console/keypad/TerminalNumericKeypadCellNumberConverter.java
+++ src/main/java/academy/devonline/tictactoe/component/console/keypad/TerminalNumericKeypadCellNumberConverter.java
@@ -20,6 +20,8 @@ package academy.devonline.tictactoe.component.console.keypad;
 import academy.devonline.tictactoe.component.console.CellNumberConverter;
 import academy.devonline.tictactoe.model.game.Cell;
 
+import static java.lang.String.format;
+
 /**
  * @author devonline
  * @link http://devonline.academy/java
@@ -28,12 +30,27 @@ public class TerminalNumericKeypadCellNumberConverter implements CellNumberConve
 
     @Override
     public Cell toCell(final char number) {
-        final int val = number - '0' - 1;
-        return new Cell(val / 3, val % 3);
+        if (number >= '1' && number <= '9') {
+            final int val = number - '0' - 1;
+            return new Cell(val / 3, val % 3);
+        } else {
+            throw new IllegalArgumentException(
+                    format("Number parameter must be between '1' and '9'! Current value is '%s'!", number)
+            );
+        }
     }
 
     @Override
     public char toNumber(final Cell cell) {
-        return (char) ('0' + (cell.getRow() * 3 + cell.getCol() + 1));
+        if (cell.getRow() >= 0 && cell.getRow() <= 2 && cell.getCol() >= 0 && cell.getCol() <= 2) {
+            return (char) ('0' + (cell.getRow() * 3 + cell.getCol() + 1));
+        } else {
+            throw new IllegalArgumentException(
+                    format(
+                            "Row and col indexes must be between 0 and 2! Current row is %s, current col is %s!",
+                            cell.getRow(), cell.getCol()
+                    )
+            );
+        }
     }
 }
